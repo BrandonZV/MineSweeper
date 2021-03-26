15bdef3b0e443e07bbddd01f0f1aadd3a5031ae9
BrandonZV <brandonzv@gmail.com>
2021-03-26 21:22:31 +0000

    Started code, saved links on alg0 and minesweeper

---
+++ main.py
@@ -0,0 +1,14 @@
+#https://www.geeksforgeeks.org/backtracking-algorithms/
+#https://www.geeksforgeeks.org/category/algorithm/backtracking/
+#https://www.geeksforgeeks.org/minesweeper-solver/
+
+def ifvalid(n):
+  for x in range (3):
+    for y in range(3):
+      if 0 <= x < n and mine[dxy[x]][dxy[y]]
+
+def main():
+  dxy = [-1, 0, 1]
+
+if __name__ == "__main__":
+  main()
